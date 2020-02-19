5dbd69a95857c6493c00f0270ea22f5b4352599f
Néstor Rodríguez <nestorandres1@gmail.com>
2020-02-19 13:50:04 -0500

    First Commit: Setup and initialize project

---
+++ Formulario-JPA/src/main/java/com/nestor/aplicacion/FormularioJpaApplication.java
@@ -0,0 +1,13 @@
+package com.nestor.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioJpaApplication.class, args);
+	}
+
+}
